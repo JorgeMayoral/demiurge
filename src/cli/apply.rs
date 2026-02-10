ff2aeece4095792a5a42b9a119c7f66c002b9d99
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2026-02-10 18:58:33 +0100

    feat: add apply from json or yaml feature

--- src/cli/apply.rs
+++ src/cli/apply.rs
@@ -8,6 +8,7 @@ use crate::{
     config::{Demiurge, DemiurgeConfig},
 };
 
+#[allow(clippy::struct_excessive_bools)]
 #[derive(Debug, Args, Clone)]
 pub struct ApplyArgs {
     /// Show the list of changes that would be made without applying them.
@@ -22,11 +23,25 @@ pub struct ApplyArgs {
     /// Allows overwriting already existing dotfile symlinks
     #[arg(long)]
     overwrite_symlink: bool,
+    /// Read the configuration from a JSON file
+    #[arg(long, conflicts_with = "from_yaml")]
+    from_json: bool,
+    /// Read the configuration from a YAML file
+    #[arg(long)]
+    from_yaml: bool,
 }
 
 impl ApplyArgs {
     pub fn run(self) -> Result<()> {
-        let configs = Demiurge::from_file(self.file)?;
+        let configs: Demiurge = if self.from_json {
+            let data = std::fs::read_to_string(self.file)?;
+            serde_json::from_str(&data)?
+        } else if self.from_yaml {
+            let data = std::fs::read_to_string(self.file)?;
+            serde_norway::from_str(&data)?
+        } else {
+            Demiurge::from_file(self.file)?
+        };
         let config = configs
             .get(&self.name)
             .context(format!("Configuration \"{}\" not found", self.name))?;
