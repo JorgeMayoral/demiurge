75d493467f4a2af5832aebd97d93977e05339337
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2026-03-01 09:09:14 +0100

    test: add utils tests

--- src/utils.rs
+++ src/utils.rs
@@ -15,3 +15,38 @@ pub fn path_tilde_expand(path: PathBuf) -> PathBuf {
 
     path
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::path_tilde_expand;
+
+    #[test]
+    fn tilde_prefix_expands_to_home() {
+        let result = path_tilde_expand("~/Documents/notes.txt".into());
+        let home = directories::UserDirs::new().unwrap();
+        assert_eq!(result, home.home_dir().join("Documents/notes.txt"));
+    }
+
+    #[test]
+    fn tilde_alone_expands_to_home_dir() {
+        let result = path_tilde_expand("~".into());
+        let home = directories::UserDirs::new().unwrap();
+        assert_eq!(result, home.home_dir().to_path_buf());
+    }
+
+    #[test]
+    fn absolute_path_without_tilde_is_unchanged() {
+        let path = PathBuf::from("/usr/local/bin");
+        let result = path_tilde_expand(path.clone());
+        assert_eq!(result, path);
+    }
+
+    #[test]
+    fn relative_path_without_tilde_is_unchanged() {
+        let path = PathBuf::from("some/relative/path");
+        let result = path_tilde_expand(path.clone());
+        assert_eq!(result, path);
+    }
+}
